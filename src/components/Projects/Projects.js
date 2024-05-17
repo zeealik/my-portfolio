bb9ceb04caa8a060b98f58c32c7e18f4217e3b18
zeealik <zeealik7@gmail.com>
2024-05-18 00:56:04 +0500

    project and script added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ProjectCard from './ProjectCards';
 import Particle from '../Particle';
-import tecgym from '../../Assets/Projects/tecgym.png';
+import TecGYM from '../../Assets/Projects/tecgym.png';
+import UsmaniaAndSons from '../../Assets/Projects/usmania-and-sons.png';
 
 function Projects() {
 	return (
@@ -15,14 +16,26 @@ function Projects() {
 				<p style={{ color: 'white' }}>
 					Here are a few projects I've worked on recently.
 				</p>
-				<Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
-					<Col md={4} className='project-card'>
+				<Row
+					style={{ justifyContent: 'center', paddingBottom: '10px' }}
+					className='row-cols-1 row-cols-md-2 row-cols-lg-2 g-4'
+				>
+					<Col className='project-card'>
 						<ProjectCard
-							imgPath={tecgym}
+							imgPath={TecGYM}
 							isBlog={false}
 							title='TECGYM'
 							description="It streamlines Gym and fitness centers management you can have control in the palm of your hand with “24/7 Mobile Monitoring”, When the dues are due, we've got your back with friendly reminders through our “whatsapp and sms services”, and much more!"
 							demoLink='http://tecfolk.com/Projects/Tecgym.html'
+						/>
+					</Col>
+					<Col className='project-card'>
+						<ProjectCard
+							imgPath={UsmaniaAndSons}
+							isBlog={false}
+							title='Usmania & Sons'
+							description='Leading real state management tool for all kind of properties'
+							demoLink='https://usmaniaandsons.com/login-signup'
 						/>
 					</Col>
 				</Row>
